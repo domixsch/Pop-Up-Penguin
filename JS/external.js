8c060f209f9ec24cee6b22b6c2c0a355a3d6c7d6
geekayush <srivastavs61@gmail.com>
2017-06-18 00:52:12 +0530

    First commit/Add files

---
+++ JS/external.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#slot1').click(function () {
+        $('#slot1').addClass('penguin1-reveal');
+    });
+    $('#slot2').click(function () {
+        $('#slot2').addClass('penguin2-reveal');
+    });
+    $('#slot3').click(function () {
+        $('#slot3').addClass('penguin3-reveal');
+    });
+    $('#slot4').click(function () {
+        $('#slot4').addClass('penguin4-reveal');
+    });
+    $('#slot5').click(function () {
+        $('#slot5').addClass('penguin5-reveal');
+    });
+    $('#slot6').click(function () {
+        $('#slot6').addClass('penguin6-reveal');
+    });
+    $('#slot7').click(function () {
+        $('#slot7').addClass('penguin7-reveal');
+    });
+    $('#slot8').click(function () {
+        $('#slot8').addClass('penguin8-reveal');
+    });
+    $('#slot9').click(function () {
+        $('#slot9').addClass('yeti-reveal');
+    });
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# POP-UP-PENGUIN Game
+
+Made using HTML, CSS and JS.
+It is a simple game to play with your friends and family. See if you can find all the penguins without waking the yeti!
